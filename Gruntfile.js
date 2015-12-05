8d34953dcf299ea79b2ddea84db062a2bbbfd5d9
Jobayer Arman <carbonjha@gmail.com>
2015-12-06 04:00:10 +0600

    update 0.2.1
    - cleaning css & js before compile
    - run jshint before & after
    - more variable used in gruntfile
    - browser prefix now works
    - minor fix in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,142 +1,154 @@
 module.exports = function(grunt) {
 
-    // constants for various paths and files to be used by the task configuration
-    var BUILD_DIR      = 'dist/';
+	// constants for various paths and files to be used by the task configuration
+	var BUILD_DIR      = 'dist/';
 
-    var BUILD_DIR_JS   = BUILD_DIR     + 'js/';
-    var BUILD_FILE_JS  = BUILD_DIR_JS  + 'script.js';
+	var BUILD_DIR_JS   = BUILD_DIR     + 'js/';
+	var BUILD_FILE_JS  = BUILD_DIR_JS  + 'script.js';
+	var BUILD_FILES_JS  = BUILD_DIR_JS  + '*.js';
 
-    var BUILD_DIR_CSS  = BUILD_DIR     + 'css/';
-    var BUILD_FILE_CSS = BUILD_DIR_CSS + 'style.min.css';
+	var BUILD_DIR_CSS  = BUILD_DIR     + 'css/';
+	var BUILD_FILE_CSS = BUILD_DIR_CSS + 'style.min.css';
+	var BUILD_FILES_CSS = BUILD_DIR_CSS + '*.css';
 
 
-    var SRC_DIR        = 'src/';
+	var SRC_DIR        = 'src/';
 
-    var SRC_DIR_JS     = SRC_DIR + 'js/';
-    var SRC_DIR_CSS    = SRC_DIR + 'css/';
-    var SRC_DIR_LESS   = SRC_DIR + 'less/';
+	var SRC_DIR_JS     = SRC_DIR + 'js/';
+	var SRC_DIR_CSS    = SRC_DIR + 'css/';
+	var SRC_DIR_LESS   = SRC_DIR + 'less/';
 
-    var SRC_FILES_JS   = SRC_DIR_JS   + '*.js';
-    var SRC_FILES_CSS  = SRC_DIR_CSS  + '*.css';
-    var SRC_FILES_LESS = SRC_DIR_LESS + '*.less';
+	var SRC_FILES_JS   = SRC_DIR_JS   + '*.js';
+	var SRC_FILES_CSS  = SRC_DIR_CSS  + '*.css';
+	var SRC_FILES_LESS = SRC_DIR_LESS + '*.less';
 
-    var AP_BROWSERS = [
-        'Android >= 4',
-        'Chrome >= 35',
-        'Firefox >= 35',
-        'Explorer >= 7',
-        'iOS >= 6',
-        'Opera >= 20',
-        'Safari >= 9'
-    ];
+	var AP_BROWSERS = [
+		'Android >= 4',
+		'Chrome >= 35',
+		'Firefox >= 35',
+		'Explorer >= 7',
+		'iOS >= 6',
+		'Opera >= 20',
+		'Safari >= 9'
+	];
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-        cssflow: {
-            options: {
-                preprocessor: 'less',
-                autoprefixer: AP_BROWSERS
-            },
-            build: {
-                files: {
-                    'src/css/style.css': 'src/less/main.less'
-                }
-            }
-        },
+		cssflow: {
+			options: {
+				preprocessor: 'less',
+				autoprefixer: {
+					browsers: AP_BROWSERS
+				}
+			},
+			build: {
+				files: {
+					'src/css/style.css': 'src/less/main.less'
+				}
+			}
+		},
 
-        copy: {
-            build: {
-                cwd: SRC_DIR_CSS,
-                src: ['*.css'],
-                dest: BUILD_DIR_CSS,
-                expand: true
-            }
-        },
+		clean: {
+			css: [BUILD_FILES_CSS, SRC_FILES_CSS],
+			js : [BUILD_FILES_JS]
+		},
 
-        less: {
-            build: {
-                files: {
-                    'src/css/style.css': SRC_FILES_LESS
-                }
-            }
-        },
+		copy: {
+			build: {
+				cwd: SRC_DIR_CSS,
+				src: ['*.css'],
+				dest: BUILD_DIR_CSS,
+				expand: true
+			}
+		},
 
-        uncss: {
-            build: {
-                files: [
-                    { src: 'index.html', dest: 'dist/css/style.uncss.css' }
-                ],
-                options: {
-                    ignoreSheets: [/fonts.googleapis/]
-                }
-            }
-        },
+		less: {
+			build: {
+				files: {
+					'src/css/style.css': SRC_FILES_LESS
+				}
+			}
+		},
 
-        cssmin: {
-            options: {
-                keepSpecialComments: 0
-            },
-            build: {
-                files: {
-                    'dist/css/style.min.css': 'src/css/style.css'
-                }
-            },
-            dist: {
-                files: {
-                    'dist/css/style.uncss.css': 'dist/css/style.uncss.css'
-                }
-            }
-        },
+		uncss: {
+			build: {
+				files: [
+					{ src: 'index.html', dest: 'dist/css/style.uncss.css' }
+				],
+				options: {
+					ignoreSheets: [/fonts.googleapis/]
+				}
+			}
+		},
 
-        jshint: {
-            options: {
-                curly: true
-            },
-            beforeconcat: [SRC_FILES_JS],
-            afterconcat: ['dist/js/script.js']
-        },
+		cssmin: {
+			options: {
+				keepSpecialComments: 0
+			},
+			build: {
+				files: {
+					'dist/css/style.min.css': 'src/css/style.css'
+				}
+			},
+			dist: {
+				files: {
+					'dist/css/style.uncss.css': 'dist/css/style.uncss.css'
+				}
+			}
+		},
 
-        concat: {
-            options: {
-                seperator: ";"
-            },
-            build: {
-                src: ['src/js/*.js'],
-                dest: 'dist/js/script.js'
-            }
-        },
+		jshint: {
+			options: {
+				curly: true,
+				globals: {
+					jQuery: true
+				}
+			},
+			beforeconcat: [SRC_FILES_JS],
+			afterconcat: [BUILD_FILE_JS]
+		},
 
-        uglify: {
-            build: {
-                files: {
-                    "dist/js/script.min.js": 'dist/js/script.js'
-                }
-            }
-        },
+		concat: {
+			options: {
+				seperator: ";"
+			},
+			build: {
+				src: [SRC_FILES_JS],
+				dest: BUILD_FILE_JS
+			}
+		},
 
-        watch: {
-            styles: {
-                options: {
-                    spawn: false
-                },
-                files: ['src/less/*.less', 'src/less/**/*.less'],
-                tasks: ['cssflow', 'copy'],
-            },
-            scripts: {
-                options: {
-                    spawn: false
-                },
-                files: ['src/js//*.js'],
-                tasks: ['concat', 'uglify', 'jshint']
-            }
-        }
-    });
+		uglify: {
+			build: {
+				files: {
+					'dist/js/script.min.js': 'dist/js/script.js'
+				}
+			}
+		},
 
-    // Load the plugins
-    require('load-grunt-tasks')(grunt);
+		watch: {
+			styles: {
+				options: {
+					spawn: false
+				},
+				files: ['src/less/*.less', 'src/less/**/*.less'],
+				tasks: ['clean:css', 'cssflow', 'copy']
+			},
+			scripts: {
+				options: {
+					spawn: false
+				},
+				files: ['src/js//*.js'],
+				tasks: ['jshint:beforeconcat', 'clean:js', 'concat', 'uglify', 'jshint:afterconcat']
+			}
+		}
+	});
 
-    //Default Task(s)
-    grunt.registerTask('default', 'watch');
-    grunt.registerTask('cleancss', ['uncss', 'cssmin:dist']);
+	// Load the plugins
+	require('load-grunt-tasks')(grunt);
+
+	//Default Task(s)
+	grunt.registerTask('default', 'watch');
+	grunt.registerTask('cleancss', ['uncss', 'cssmin:dist']);
 };
